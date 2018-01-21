f037c1b839e21636117c9628a9bb434d4f3decb1
genius22222 <alexander.archibasov.mgn@yandex.ru>
2018-01-21 15:29:15 +0500

    *Create first file plugin

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Галлерея TootKot
+Plugin URI: https://github.com/genius22222/tok-gallery
+Description: Плагин галлереи для темы TootKot-v-2-0
+Version: 1.0
+Author: Alexander Archibasov
+Author URI: https://vk.com/id89802582
+*/
+?>
